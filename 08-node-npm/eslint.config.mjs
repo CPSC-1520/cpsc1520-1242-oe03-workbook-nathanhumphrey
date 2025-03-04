5fd183b9a3441dd2452b9363c7db3150ee008fcc
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-03 22:09:36 -0700

    Week 08 NPM Update ESLint Config

--- 08-node-npm/eslint.config.mjs
+++ 08-node-npm/eslint.config.mjs
@@ -1,16 +1,13 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import stylisticJs from '@stylistic/eslint-plugin-js';
-
+import stylistic from '@stylistic/eslint-plugin';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    pluginJs.configs.recommended,
-    stylisticJs.configs.all,
-    {languageOptions: {'globals': globals.browser},
-        rules: {'@stylistic/js/quotes': [
-            'error',
-            'single'
-        ],
-        '@stylistic/js/quote-props': 'off'}}
+  pluginJs.configs.recommended,
+  stylistic.configs.recommended,
+  { languageOptions: { globals: globals.browser },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+    } },
 ];
