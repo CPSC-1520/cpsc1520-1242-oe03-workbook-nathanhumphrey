bec2996784167c84e2ec50e0d5be6c282c207378
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-05 20:35:47 -0700

    Week 08 Modules Static Asset Limit

--- 08-modules/js/main.js
+++ 08-modules/js/main.js
@@ -1,4 +1,24 @@
 import { getTemps } from './utils.js';
-const temps = await getTemps();
+import Chart from 'chart.js/auto';
 
-console.log(temps);
+(async () => {
+  const temps = await getTemps();
+
+  console.log(temps);
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: temps.map(row => row.month),
+        datasets: [
+          {
+            label: 'Avg Monthly Temps for 2025',
+            data: temps.map(row => row.temp),
+          },
+        ],
+      },
+    },
+  );
+})();
