20ec25489de2c7bfbd8844442e1355fcbc1469fb
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-17 21:17:25 -0600

    Week 10 Rick & Morty Fetch Complete

--- 10-rick-and-morty-fetch/js/main.js
+++ 10-rick-and-morty-fetch/js/main.js
@@ -1,8 +1,27 @@
-const endpoint = 'https://uselessfacts.jsph.pl/api/v2/facts/random';
+const endpoint = 'https://rickandmortyapi.com/api/character/?name=';
 
-document.querySelector('#btn-fact').addEventListener('click', async () => {
-  const response = await fetch(endpoint);
-  const fact = await response.json();
-  
-  document.querySelector('#fact-text').innerText = fact.text;
+document.querySelector('#frm-search').addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const searchValue = evt.target.elements.search.value;
+  const response = await fetch(`${endpoint}${searchValue}`);
+
+  if (response.status == 200) {
+    const {results} = await response.json();
+    document.querySelector('tbody').innerHTML = results.map(createCharacterRow).join('');
+  } else {
+    document.querySelector('tbody').innerHTML = '<tr><td colspan="3">No results found.</td></tr>';
+  }
 });
+
+/**
+ * Creates the row markup for a character.
+ * @param {Object} char - a Rick & Morty character
+ * @returns A row for the table
+ */
+function createCharacterRow(char) {
+  return `<tr>
+            <td><img src="${char.image}" alt="${char.name}"></td>
+            <td>${char.name}</td>
+            <td>${char.status}</td>
+          </tr>`;
+}
