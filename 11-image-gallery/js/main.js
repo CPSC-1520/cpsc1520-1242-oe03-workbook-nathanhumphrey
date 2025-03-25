37867769e8a3380f887f2d323b19355256e3f486
Nathan Humphrey <NathanH@nait.ca>
2025-03-24 20:08:10 -0600

    Week 11 Prev and Next Logic

--- 11-image-gallery/js/main.js
+++ 11-image-gallery/js/main.js
@@ -1,18 +1,68 @@
 const images = [
-    'img/beach1.jpg',
-    'img/beach2.jpg',
-    'img/beach3.jpg',
-];
+  'img/beach1.jpg', // 0
+  'img/beach2.jpg', // 1
+  'img/beach3.jpg', // 2
+]; // length 3
 
-document.querySelector('#next-img').addEventListener('click', () => {
-    document.querySelector('.gallery img').src = images[1];
+let currentImageIndex = 0;
+
+document.querySelector('.controls').addEventListener('click', (evt) => {
+  const target = evt.target;
+  // Wrap around solution
+  switch (target.id) {
+    case 'prev-img':
+      currentImageIndex -= 1;
+      if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+      }
+      displayImage(currentImageIndex);
+
+      break;
+    case 'next-img':
+      currentImageIndex += 1;
+
+      if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+      }
+      displayImage(currentImageIndex);
+
+      break;
+    default:
+      console.log(`${target} clicked...`);
+      break;
+  }
+
+  // Stop at the ends solution
+  // switch (target.id) {
+  //   case 'prev-img':
+  //     if (currentImageIndex > 0) {
+  //       currentImageIndex -= 1;
+  //       displayImage(currentImageIndex);
+  //     }
+
+  //     break;
+  //   case 'next-img':
+  //     if (currentImageIndex < images.length - 1) {
+  //       currentImageIndex += 1;
+  //       displayImage(currentImageIndex);
+  //     }
+
+  //     break;
+  //   default:
+  //     console.log(`${target} clicked...`);
+  //     break;
+  // }
 });
 
+function displayImage(index) {
+  document.querySelector('.gallery img').src = images[index];
+}
+
 function preloadImages(imgArray) {
-    imgArray.forEach(imgSrc => {
-        const img = new Image();
-        img.src = imgSrc;
-    });
+  imgArray.forEach(imgSrc => {
+    const img = new Image();
+    img.src = imgSrc;
+  });
 }
 
 preloadImages(images);
