bf1c0c59270b91168d47a594fb1d566fb03e5065
Nathan Humphrey <NathanH@nait.ca>
2025-03-24 21:02:58 -0600

    Week 11 Slideshow Play and Pause and Keys

--- 11-image-gallery/js/main.js
+++ 11-image-gallery/js/main.js
@@ -5,27 +5,51 @@ const images = [
 ]; // length 3
 
 let currentImageIndex = 0;
+let intervalId;
+let isSlideshowPlaying = true;
+
+document.addEventListener('keydown', (evt) => {
+  switch (evt.code) {
+    case 'ArrowLeft':
+      updateCurrentImage(-1);
+      break;
+    case 'ArrowRight':
+      updateCurrentImage(+1);
+      break;
+    case 'Space':
+      if (isSlideshowPlaying) {
+        stopSlideshow();
+        isSlideshowPlaying = false;
+        document.querySelector('#pause-img').innerText = 'Play';
+      } else {
+        startSlideshow();
+        isSlideshowPlaying = true;
+        document.querySelector('#pause-img').innerText = 'Pause';
+      }
+      break;
+  }
+});
 
 document.querySelector('.controls').addEventListener('click', (evt) => {
   const target = evt.target;
   // Wrap around solution
   switch (target.id) {
     case 'prev-img':
-      currentImageIndex -= 1;
-      if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-      }
-      displayImage(currentImageIndex);
-
+      updateCurrentImage(-1);
       break;
     case 'next-img':
-      currentImageIndex += 1;
-
-      if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
+      updateCurrentImage(+1);
+      break;
+    case 'pause-img':
+      if (isSlideshowPlaying) {
+        stopSlideshow();
+        isSlideshowPlaying = false;
+        target.innerText = 'Play';
+      } else {
+        startSlideshow();
+        isSlideshowPlaying = true;
+        target.innerText = 'Pause';
       }
-      displayImage(currentImageIndex);
-
       break;
     default:
       console.log(`${target} clicked...`);
@@ -54,8 +78,35 @@ document.querySelector('.controls').addEventListener('click', (evt) => {
   // }
 });
 
+function updateCurrentImage(direction) {
+  if (direction > 0) {
+    currentImageIndex += 1;
+    if (currentImageIndex >= images.length) {
+      currentImageIndex = 0;
+    }
+    displayImage(currentImageIndex);
+  } else {
+    currentImageIndex -= 1;
+    if (currentImageIndex < 0) {
+      currentImageIndex = images.length - 1;
+    }
+    displayImage(currentImageIndex);
+  }
+}
+
 function displayImage(index) {
   document.querySelector('.gallery img').src = images[index];
+}
+
+function startSlideshow() {
+  // Set an interval to run through the slideshow
+  intervalId = setInterval(() => {
+    updateCurrentImage(+1);
+  }, 3000);
+}
+
+function stopSlideshow() {
+  clearInterval(intervalId);
 }
 
 function preloadImages(imgArray) {
@@ -65,4 +116,8 @@ function preloadImages(imgArray) {
   });
 }
 
+// Preload images
 preloadImages(images);
+
+// Start the show
+startSlideshow();
