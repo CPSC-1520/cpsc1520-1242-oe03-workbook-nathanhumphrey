c57329188f286e3a72c41ec35f9d91e23303efc3
Nathan Humphrey <nathpaulhump@gmail.com>
2025-03-31 20:52:16 -0600

    Week 12 Todos Rendered

--- 12-todo-dom-api-npm/frontend/js/main.js
+++ 12-todo-dom-api-npm/frontend/js/main.js
@@ -1,22 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { fetchTodos } from './utils.js';
 
-fetchTodos();
+init();
 
-`
-<ul class="list-group">
-  <li class="list-group-item d-flex justify-content-between align-items-start">
+async function init() {
+  const todos = await fetchTodos();
+  const todosList = document.querySelector('#todos');
+  todosList.innerHTML = todos.map(renderTodo).join('');
+}
+
+function renderTodo(todo) {
+  return `
+  <li data-todo-id="${todo.id}" class="list-group-item d-flex justify-content-between align-items-start">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Default checkbox
+      <input class="form-check-input" type="checkbox" ${todo.complete ? 'checked' : ''} id="todo${todo.id}">
+      <label class="form-check-label" for="todo${todo.id}">
+        ${todo.description}
       </label>
     </div>
     <span>
-      <button class="btn">Up</button>
-      <button class="btn">Down</button>
-      <button class="btn btn-danger>Delete</button>
+      <button class="btn btn-primary up">Up</button>
+      <button class="btn btn-primary down">Down</button>
+      <button class="btn btn-danger delete">Delete</button>
     </span>
   </li>
-</ul>
-`;
+  `;
+}
