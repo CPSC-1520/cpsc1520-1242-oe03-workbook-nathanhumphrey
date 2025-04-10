89eefbb773b9e67969801cffdee9a539ac9a7b8b
Nathan Humphrey <nathpaulhump@gmail.com>
2025-04-09 21:26:36 -0600

    Week 13  Classes Add Event Listeners

--- 13-classes/js/utils/card.js
+++ 13-classes/js/utils/card.js
@@ -5,12 +5,31 @@ class Card {
     this.element = createCardElement({ title, description });
     this.toLearnList = document.querySelector('.to-learn-stickies');
     this.understoodList = document.querySelector('.understood-stickies');
-
+    this.addEventListeners();
     this.render();
   }
 
   render() {
     this.toLearnList.appendChild(this.element);
+  }
+
+  addEventListeners() {
+    const cardBody = this.element.firstElementChild;
+    const removeButton = cardBody.querySelector('.btn-danger');
+    const topPriorityButton = removeButton.nextElementSibling;
+    const understoodButton = cardBody.children[4];
+
+    removeButton.addEventListener('click', () => {
+      console.log('Remove clicked');
+    });
+
+    topPriorityButton.addEventListener('click', () => {
+      console.log('TopPriority clicked');
+    });
+
+    understoodButton.addEventListener('click', () => {
+      console.log('Understood clicked');
+    });
   }
 }
 
